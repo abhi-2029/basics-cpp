191d8eae96d7bc977f3da8d34f731e387ec8832a
Abhishek Ranjan <119099659+abhi-2029@users.noreply.github.com>
2024-03-16 15:18:47 +0530

    Add files via upload

---
+++ area_of_sphere.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int radius=3;
+    int pi=3.14;
+    float area;
+    area=pi*(radius*radius);
+    cout<<"Area of circle is: "<<area<<endl;
+    return 0;
+}
